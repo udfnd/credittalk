d5584bddd2bd7a62b4dd62038d8219c811f4ddc7
DeepLe_UnDeFined <hsm9300@deeple.page>
2025-06-10 00:55:34 +0900

    20250610

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: true, // 세미콜론 추가
-  singleQuote: true, // 작은 따옴표 사용
-  trailingComma: 'all', // 후행 쉼표 추가
-  printWidth: 80, // 줄 길이 80자 제한
-  tabWidth: 2, // 탭 너비 2
-  useTabs: false, // 탭 대신 스페이스 사용
-};
