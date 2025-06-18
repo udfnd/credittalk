05403e412eb36c9e5147f411d98a5fc231ca7f1c
udfnd <hsm9300@naver.com>
2025-06-18 18:38:17 +0900

    20250618

--- ios/credittalk-Bridging-Header.h
+++ ios/credittalk-Bridging-Header.h
@@ -1,16 +1,13 @@
-//
-//  credittalk-Bridging-Header.h
-//  credittalk
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
 
-#import <kakao-login/RNKakaoLogins.h>
+// React Deprecation 모듈 맵 로드
+#import <RCTDeprecation/RCTDeprecation.h>
+
+// Kakao 로그인
+#import <RNKakaoLogins.h>
+
+// Naver 로그인
 #import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
-
-
