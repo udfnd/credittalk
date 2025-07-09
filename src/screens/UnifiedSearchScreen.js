6abde41f19d943ae33e3cfcb02520114c90ab89d
udfnd <hsm9300@naver.com>
2025-07-10 01:08:51 +0900

    20250710

--- src/screens/UnifiedSearchScreen.js
+++ src/screens/UnifiedSearchScreen.js
@@ -3,7 +3,7 @@ import SearchBaseScreen from './SearchBaseScreen'; // 경로 확인
 
 function UnifiedSearchScreen() {
   return (
-    <SearchBaseScreen title="통합 검색" />
+    <SearchBaseScreen title="피해 사례 검색" />
   );
 }
 export default UnifiedSearchScreen;
