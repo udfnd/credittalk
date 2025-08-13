0acfb0e1f87d3f02a60f690c99ace802e12858c7
udfnd <hsm9300@naver.com>
2025-08-13 22:13:12 +0900

    20250813

--- ios/credittalk/AppDelegate.swift
+++ ios/credittalk/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     window = UIWindow(frame: UIScreen.main.bounds)
     factory.startReactNative(
-      withModuleName: "CreditTalk",
+      withModuleName: "credittalk",
       in: window,
       launchOptions: launchOptions
     )
