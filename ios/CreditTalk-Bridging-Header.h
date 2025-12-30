0428d0627e6e9e7444fee51cd5450a6e1cdcbb24
udfnd <hsm9300@naver.com>
2025-12-30 20:16:52 +0900

    20251230

--- ios/CreditTalk-Bridging-Header.h
+++ ios/CreditTalk-Bridging-Header.h
@@ -1,13 +1,41 @@
+// React Native 0.79+ with use_frameworks! :linkage => :static
+// Objective-C 헤더는 브리징 헤더를 통해 접근합니다.
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// React-RCTAppDelegate Headers
+#if __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
+#import <React_RCTAppDelegate/RCTAppDelegate.h>
+#import <React_RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>
+#import <React_RCTAppDelegate/RCTReactNativeFactory.h>
+#import <React_RCTAppDelegate/RCTDependencyProvider.h>
+#elif __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
+#import <React-RCTAppDelegate/RCTAppDelegate.h>
+#import <React-RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>
+#import <React-RCTAppDelegate/RCTReactNativeFactory.h>
+#import <React-RCTAppDelegate/RCTDependencyProvider.h>
+#elif __has_include("RCTAppDelegate.h")
+#import "RCTAppDelegate.h"
+#import "RCTDefaultReactNativeFactoryDelegate.h"
+#import "RCTReactNativeFactory.h"
+#import "RCTDependencyProvider.h"
+#endif
+
+// ReactAppDependencyProvider
+#if __has_include(<ReactAppDependencyProvider/RCTAppDependencyProvider.h>)
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+#elif __has_include("RCTAppDependencyProvider.h")
+#import "RCTAppDependencyProvider.h"
+#endif
+
+// React Native Core Headers
+#if __has_include(<React/RCTBridge.h>)
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
-
-// React Deprecation 모듈 맵 로드
-#import <RCTDeprecation/RCTDeprecation.h>
-
-// Kakao 로그인
-#import <RNKakaoLogins.h>
-
-// Naver 로그인
-#import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
+#elif __has_include("RCTBridge.h")
+#import "RCTBridge.h"
+#import "RCTBundleURLProvider.h"
+#import "RCTLinkingManager.h"
+#endif
